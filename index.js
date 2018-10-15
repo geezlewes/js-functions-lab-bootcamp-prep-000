0cf659b61d666ade0116e155f21f4721c2169f08
Luis Rafael Santiago Villalta <santiagovillalta@outlook.com>
2018-10-15 02:16:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // write your code below!
+var happyHolidays = "happy holidays!"
 function happyHolidays {
   return "happy holidays!"
 }
 function happyHolidaysTo (name){
-  return 'happy holidays , ${name}!'
+  return 'Happy holidays , ${name}!'
 }
-function happyHolidaTo(holiday,name){
-  return 'happy ${holiday},${name}!'
+function happyHolidayTo(holiday,name){
+  return 'Happy ${holiday},${name}!'
 }
-function holidayCountdown (holiday,days)
+function holidayCountdown (holiday,days){
 return 'It's ${days} days until ${holiday}!''
+}
